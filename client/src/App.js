000ac539f3fbffe3b2393b54d4496f95638eee06
AlessandroDeFa <aledefaale@gmail.com>
2023-01-29 20:08:29 +0100

    created components and pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App">{/* mettere le route react router dom  */}</div>;
 }
+
+// nav bar con sezioni:
+// -header: nome account: ospite tasto con black e white mode
+// -dashboard: con bilancio totale + opzione di aggiungere una trandazione + trasazioni passate
+// -statistiche: guardare dashboard sito pirateking
+// -pianificazione(da decidere nel tempo): guardare app wallet su pianificarione
 
 export default App;
