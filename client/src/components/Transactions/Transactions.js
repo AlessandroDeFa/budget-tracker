286f41dcc83bfd0d86028ef1a92338c2826ea460
AlessandroDeFa <aledefaale@gmail.com>
2023-02-06 18:23:35 +0100

    updated Transactions.js

--- client/src/components/Transactions/Transactions.js
+++ client/src/components/Transactions/Transactions.js
@@ -59,6 +59,7 @@ export const Transactions = () => {
           setNewCategory("");
           setNewName("");
           setNewNote("");
+          setFormUpdate(false);
           setSuccessUp(response);
         })
         .catch((error) => {
